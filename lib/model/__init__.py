6425f26d775595995ec031ad36b9973d0e512853
ashok-vardan <140464241+ashok-vardan@users.noreply.github.com>
2023-07-27 02:23:23 +0530

    Update __init__.py

--- lib/model/__init__.py
+++ lib/model/__init__.py
@@ -1,5 +1,85 @@
-# Copyright (c) Facebook, Inc. and its affiliates. All rights reserved.
-
+import torch
+import torch.nn as nn
+import torch.optim as optim
 from .MLP import MLP
-from .HGPIFuMRNet import HGPIFuMRNet
 from .HGPIFuNetwNML import HGPIFuNetwNML
+
+# Define the Generator
+class Generator(nn.Module):
+    def __init__(self, input_dim, output_dim, hidden_channels, merge_layer=0, res_layers=[], norm='group'):
+        super(Generator, self).__init__()
+        self.mlp = MLP(input_dim, output_dim, hidden_channels, merge_layer=merge_layer, res_layers=res_layers, norm=norm)
+
+    def forward(self, noise):
+        # Generate 3D shapes from input noise
+        return self.mlp(noise)
+
+# Define the Discriminator
+class Discriminator(HGPIFuNetwNML):
+    def __init__(self, input_dim, mlp_hidden_channels, merge_layer=0, res_layers=[], norm='group', **kwargs):
+        super(Discriminator, self).__init__(**kwargs)
+        self.mlp = MLP(input_dim, 1, mlp_hidden_channels, merge_layer=merge_layer, res_layers=res_layers, norm=norm)
+
+    def forward(self, points, calibs, transforms=None):
+        # Calculate surface normal in 'model' space using the HGPIFuNetwNML
+        nmls = self.calc_normal(points, calibs, transforms)
+        # Pass the calculated normals to the MLP Discriminator
+        return self.mlp(nmls)
+
+# Initialize the Generator
+latent_dim = 100  # You can adjust the size of the generator's input noise vector
+output_dim = 3  # Adjust this based on the dimension of the generated 3D shapes
+hidden_channels_gen = [64, 128, 256, 512]  # Adjust this based on the desired hidden layer sizes
+merge_layer_gen = 2  # Adjust this if you want the generator to provide intermediate features
+generator = Generator(latent_dim, output_dim, hidden_channels_gen, merge_layer_gen)
+
+# Initialize the Discriminator
+input_dim_disc = 3  # Adjust this based on the dimension of the generated 3D shapes (surface normal dimension)
+hidden_channels_disc = [64, 128, 256, 512]  # Adjust this based on the desired hidden layer sizes for the discriminator
+merge_layer_disc = 2  # Adjust this if you want the discriminator to provide intermediate features
+discriminator = Discriminator(input_dim_disc, hidden_channels_disc, merge_layer_disc)
+
+# Note: You might need to adjust other hyperparameters and settings based on your specific use case and data.
+
+# Training the GAN would involve alternating optimization of the generator and discriminator using GAN loss functions. The exact training procedure and hyperparameters depend on your use case and dataset.
+
+# Here's a simple example of training loop for the GAN (you might need to adjust this based on your specific GAN loss functions and optimization settings):
+
+# Define the GAN loss function (e.g., binary cross-entropy loss)
+gan_loss_fn = nn.BCEWithLogitsLoss()
+
+# Define the optimizer for the generator and discriminator
+generator_optimizer = optim.Adam(generator.parameters(), lr=0.0002, betas=(0.5, 0.999))
+discriminator_optimizer = optim.Adam(discriminator.parameters(), lr=0.0002, betas=(0.5, 0.999))
+
+for epoch in range(num_epochs):
+    for batch_idx, (real_data, _) in enumerate(data_loader):
+        # Training the discriminator
+        real_labels = torch.ones(real_data.size(0), 1, device=device)
+        fake_labels = torch.zeros(real_data.size(0), 1, device=device)
+
+        real_output = discriminator(real_data)
+        real_loss = gan_loss_fn(real_output, real_labels)
+
+        noise = torch.randn(real_data.size(0), latent_dim, device=device)
+        fake_data, _ = generator(noise)
+
+        fake_output = discriminator(fake_data.detach())
+        fake_loss = gan_loss_fn(fake_output, fake_labels)
+
+        discriminator_loss = real_loss + fake_loss
+
+        discriminator_optimizer.zero_grad()
+        discriminator_loss.backward()
+        discriminator_optimizer.step()
+
+        # Training the generator
+        noise = torch.randn(real_data.size(0), latent_dim, device=device)
+        fake_data, _ = generator(noise)
+
+        generator_output = discriminator(fake_data)
+        generator_loss = gan_loss_fn(generator_output, real_labels)
+
+        generator_optimizer.zero_grad()
+        generator_loss.backward()
+        generator_optimizer.step()
