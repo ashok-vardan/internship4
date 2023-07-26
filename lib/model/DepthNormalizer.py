74bc4b5ff8c94ad96909cd8a599ccfcfffc9912c
ashok-vardan <140464241+ashok-vardan@users.noreply.github.com>
2023-07-27 02:30:06 +0530

    Update DepthNormalizer.py

--- lib/model/DepthNormalizer.py
+++ lib/model/DepthNormalizer.py
@@ -2,7 +2,7 @@
 
 import torch
 import torch.nn as nn
-import torch.nn.functional as F 
+import torch.nn.functional as F
 
 class DepthNormalizer(nn.Module):
     def __init__(self, opt):
@@ -11,10 +11,22 @@ class DepthNormalizer(nn.Module):
 
     def forward(self, xyz, calibs=None, index_feat=None):
         '''
-        normalize depth value
+        Normalize depth value.
         args:
             xyz: [B, 3, N] depth value
         '''
-        z_feat = xyz[:,2:3,:] * (self.opt.loadSize // 2) / self.opt.z_size
-
+        z_feat = xyz[:, 2:3, :] * (self.opt.loadSize // 2) / self.opt.z_size
         return z_feat
+
+class Generator(nn.Module):
+    # ... (other parts of the Generator)
+
+    def forward(self, x):
+        # ... (rest of the Generator code)
+
+        normx = x
+        z_feat = DepthNormalizer(opt)(xyz)  # Integrating the DepthNormalizer here
+
+        # ... (rest of the Generator code)
+
+        return outputs, normx
